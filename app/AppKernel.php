336223de7e30fca4f0bc6630a259c09b4db11e3d
kisphp <mariusbogdan83@gmail.com>
2016-07-24 09:02:50 +0000

    added database import/export bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new Kisphp\NewsletterBundle\KisphpNewsletterBundle(),
             new \Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new \Kisphp\DatabaseBundle\KisphpDatabaseBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
