de89e4a1b35c7dd153d22fe3ea3a840c526ea6c7
kisphp <mariusbogdan83@gmail.com>
2016-08-22 21:36:26 +0200

    full test of a service

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,4 +1,4 @@
 <?php
 // This is global bootstrap for autoloading
 
-require_once __DIR__ . '/../app/autoload.php';
+//require_once __DIR__ . '/../app/autoload.php';
---
+++ tests/unit/Kisphp/NewsletterBundle/Services/NewsletterServiceTest.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace tests\unit\Kisphp\NewsletterBundle\Services;
+
+use AppBundle\Utils\Status;
+use Codeception\Test\Unit;
+use Codeception\Util\Stub;
+use Doctrine\ORM\EntityManager;
+use Kisphp\NewsletterBundle\Entity\NewsletterEntity;
+use Kisphp\NewsletterBundle\Entity\Repository\NewsletterRepository;
+use Kisphp\NewsletterBundle\Services\NewsletterService;
+
+class NewsletterServiceTest extends Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    /**
+     * @var NewsletterService
+     */
+    protected $service;
+
+    protected function _before()
+    {
+
+
+        $newsletterRepository = Stub::makeEmpty(
+            NewsletterRepository::class,
+            [
+                'find' => function($i) {
+                    return $i;
+                },
+            ]
+        );
+        $entityManager = Stub::makeEmpty(
+            EntityManager::class,
+            [
+                'getRepository' => function() use ($newsletterRepository){
+                    return $newsletterRepository;
+                },
+                'persist' => function () {
+                    return;
+                },
+                'flush' => function () {
+                    return;
+                },
+            ]
+        );
+
+        $this->service = new NewsletterService($entityManager);
+    }
+
+    protected function _after()
+    {
+    }
+
+    protected function createEntity()
+    {
+        return Stub::makeEmpty(
+            NewsletterEntity::class,
+            [
+                'status' => '',
+                'setStatus' => function ($status){
+                    $this->status = $status;
+                },
+                'getStatus' => function (){
+                    return $this->status;
+                },
+            ]
+        );
+    }
+
+    // tests
+    public function test_get_newsletter_by_id()
+    {
+        $entity = $this->service
+            ->getNewsletterById(1)
+        ;
+
+        self::assertEquals(1, $entity);
+    }
+
+    public function test_save_newsletter()
+    {
+        $entity = $this->createEntity();
+
+        $output = $this->service
+            ->saveNewsletter($entity)
+        ;
+
+        self::assertNotNull($output);
+    }
+
+    public function test_delete_entity()
+    {
+        $entity = $this->createEntity();
+
+        $output = $this->service
+            ->deleteNewsletter($entity)
+        ;
+
+        self::assertEquals(Status::DELETED, $output->getStatus());
+    }
+
+    public function test_create_newsletter_state_machine_array()
+    {
+        $entity = $this->createEntity();
+        $stateMachineRules = $this->service
+            ->createNewsletterStateMachine()
+        ;
+
+        self::assertArrayHasKey('states', $stateMachineRules);
+        $stateMachineRules['callbacks']['after'][0]['do']($entity);
+    }
+
+    public function test_get_state_machine()
+    {
+        $entity = $this->createEntity();
+
+        $output = $this->service
+            ->getStateMachine($entity)
+        ;
+
+        self::assertNotNull($output);
+    }
+
+    public function test_get_pending_newsletters()
+    {
+        $output = $this->service
+            ->getPendingNewsletters()
+        ;
+
+        self::assertNull($output);
+    }
+
+    public function test_query_newsletters()
+    {
+        $output = $this->service
+            ->queryNewsletters()
+        ;
+
+        self::assertNull($output);
+    }
+}
